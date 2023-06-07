210345eb518751c28186702b73a3d9ca965f0cf1
Jagdish Thakur <104257196+JaggiEdits03@users.noreply.github.com>
2023-06-07 06:36:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=https://t.me/BackupByJaggi
+                    "🤖 Join Updates Channel", url="https://t.me/BackupByJaggi"
                 )
             ]
         ]
